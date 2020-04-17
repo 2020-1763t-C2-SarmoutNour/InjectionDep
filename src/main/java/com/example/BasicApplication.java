1e2cf5e52d70c81e98a9e7916c69f1bd773c4d73
Nour El houda Sarmout <nourelhouda.sarmout@isae.edu.lb>
2020-04-17 16:34:06 +0300

    Récupeération du projet

---
+++ src/main/java/com/example/BasicApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class BasicApplication {
+
+	public static void main(String[] args) {
+		var ctx = SpringApplication.run(BasicApplication.class, args);
+                System.out.println("# Beans: "+ ctx.getBeanDefinitionCount());
+                
+                var names = ctx.getBeanDefinitionNames();
+                Arrays.sort(names);
+                Arrays.asList(names).forEach(System.out::println);
+                
+                
+	}
+}
---
+++ src/main/java/com/example/MaClasse.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author user
+ */
+@Component
+public class MaClasse implements IClasse{
+    
+    @Override
+    public void affiche(){}
+}
